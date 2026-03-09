edb9fe8774e2602eeb557e2e6c6af585704fb886
IshwarTandi2 <ishwartandi26@gmail.com>
2026-03-09 23:23:11 +0530

    Commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,7 @@ const Login = () => {
                 <img className="" src='https://assets.nflxext.com/ffe/siteui/vlv3/75772f65-58b5-465f-b642-fa858b6168ca/web/IN-en-20260302-TRIFECTA-perspective_26418256-c5f3-4e9a-8160-a6b534228a2f_large.jpg' alt='Background image'></img>
             </div>
                 <form className="w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
-                <h1 className='text-3xl font-bold'>Sign In</h1>
+                <h1 className='text-3xl font-bold'>Sign in</h1>
                     <input className="font-bold  text-lg p-4 my-4 w-full bg-gray-700" type='text' placeholder='Email or phone number'/>
                     <input className="font-bold  text-lg p-4 my-4 w-full bg-gray-700 " type='password' placeholder='Password'/>
                     <button className=" font-bold  text-lg bg-red-700 p-4 my-4 w-full cursor-pointer">Sign In</button>
