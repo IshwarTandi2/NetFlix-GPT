c11919e87086c6269c8c3bb655545cd1286ac694
IshwarTandi2 <ishwartandi26@gmail.com>
2026-03-29 23:55:44 +0530

    updated

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -9,7 +9,7 @@ const SecondaryContainer = () => {
   return (
     <div className='bg-black'>
      <div className='-mt-72 pl-12 relative z-20'>
-      <Movielist title={"Now Playing "} movies={movies.nowPlayingMovie} />;
+      <Movielist title={"Now playing "} movies={movies.nowPlayingMovie} />;
       <Movielist title={"Popular"} movies={movies.popularMovie} />;
       <Movielist title={"Top Rated"} movies={movies.nowPlayingMovie} />;
       <Movielist title={"Upcoming"} movies={movies.upcomingMovie} />;
